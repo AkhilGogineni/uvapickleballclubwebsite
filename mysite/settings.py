9a217226b6092f7f4c2b11703d180e87845ea6f4
FuriousZen <williamwo@icloud.com>
2026-04-19 09:28:36 -0400

    debug=true for sprint today

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-%mqi8^0+l$q+$jrtamim2ew5ibwgr=z!&yhr5nwour4lr=_cq0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['swe-group8-6275098586a9.herokuapp.com', '0.0.0.0', '127.0.0.1', 'localhost', '.herokuapp.com']
 
